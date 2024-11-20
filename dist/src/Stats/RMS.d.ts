6698e712db3ba2d4d7763d6b81c5af2483bc45c0
Sam Bowden <sam.bowden@techex.co.uk>
2024-11-20 15:31:13 +0000

    chore: clean unused files from dist

--- dist/src/Stats/RMS.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class RootMeanSquareStats {
-    sumOfSquares: number;
-    count: number;
-    constructor();
-    update(sample: number): void;
-    getRootMeanSquare(): number;
-    reset(): void;
-}
--- dist/src/Utils/AudioUtils/UpdateRMS.d.ts
+++
@@ -1,4 +0,0 @@
-import { type InputParams, type MixerParams } from '../../Types/ParamTypes';
-import { type ModifiedDataView } from '../../ModifiedDataView/ModifiedDataView';
-import { type RootMeanSquareStats } from '../../Stats/RMS';
-export declare function updateRootMeanSquare(audioData: ModifiedDataView, params: InputParams | MixerParams, rms: RootMeanSquareStats): void;
